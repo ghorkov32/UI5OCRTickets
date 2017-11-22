1dbebf7699b8caf135f1e1661f16df2db685da63
Puszkiel, Ivan N <ivan.n.puszkiel@accenture.com>
2017-11-22 15:23:53 -0300

    Created cordova project, added platforms iOS, Android, and necessary plugins.

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,30 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-device.device",
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "pluginId": "cordova-plugin-device",
+        "clobbers": [
+            "device"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-console": "1.1.0",
+    "cordova-plugin-device": "1.1.7",
+    "cordova-plugin-inappbrowser": "1.7.2",
+    "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
