1f5e18f1b8579e925f3797859065aa6b6464a542
Natalia Laborde <natalia.c.laborde@accenture.com>
2017-12-19 16:50:28 -0300

    Added a fragment whith matches list and detected lines.

--- www/controller/Main.controller.js
+++ www/controller/Main.controller.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller"
 ], function(Controller) {
-	"use strict";
+    "use strict";
 
-	return Controller.extend("UI5OCRTickets.controller.Main", {
+    return Controller.extend("UI5OCRTickets.controller.Main", {
 
-		onClickTakePhoto: function (oEvent) {
+        onClickTakePhoto: function (oEvent) {
             var that = this;
             //Base64 JPG Prefix
             var JPG_BASE64_PREFIX = "data:image/jpeg;base64,";
@@ -21,73 +21,127 @@ sap.ui.define([
 
                     window.Tesseract.recognize(sImgBlob, {lang:'spa'})
                         .progress(message => {
-                            console.log(message);
-                            sap.ui.core.BusyIndicator.show();
-                        })
-                          .catch(err => console.error(err))
-                          .then(result => {
-                                  other.byId("__input5").setValue(result.text);
-                              sap.ui.core.BusyIndicator.hide();
-                        })
-                        .finally(resultOrError => {
+                        console.log(message);
+                    sap.ui.core.BusyIndicator.show();
+                })
+                .catch(err => console.error(err))
+                .then(result => {
+                        other.byId("__input5").setValue(result.text);
+                    sap.ui.core.BusyIndicator.hide();
+                })
+                .finally(resultOrError => {
 
-                            var oDate = /(\d{2}(\/|-|\s)){2}\d{2}/;
-                            var oPlace = /\b.+S(\.)?(\s)?A(\.)?/i;
-                            var sDate = resultOrError.text.match(oDate);
-                            var sPlace = resultOrError.text.match(oPlace);
+                    var oDate = /(\d{2}(?:\/|-|\s)){2}\d{2}/;
+                    var oPlace = /\b.+S(?:\.)?(?:\s)?A(?:\.)?/i;
+                    var sDate = resultOrError.text.match(oDate);
+                    var sPlace = resultOrError.text.match(oPlace);
 
-                            try {
-                              other.byId("__input0").setValue(sDate[0]);
-
-
-                            } catch(err) {
-
-                              other.byId("__input0").setValue("");
-
-                            }
-                            try {
-                              other.byId("__input5").setValue(sPlace[0]);
+                    try {
+                        other.byId("__input0").setValue(sDate[0]);
 
 
-                            } catch(err) {
+                    } catch(err) {
 
-                              other.byId("__input5").setValue("");
+                        other.byId("__input0").setValue("");
 
-                            }
+                    }
+                    try {
+                        other.byId("__input5").setValue(sPlace[0]);
 
-                            var oCuil = /\d{2}[-]\d{8}[-]\d{1}/g;
-                            var sCuil = resultOrError.text.match(oCuil);
-                            var oTotal =/(?:Total\:\s)(?:\w+\s)(\d+)(?:.|,|s)(\d+)/;
-                            var sTotal =resultOrError.text.match(oTotal);
-                            try {
-                                  other.byId("__input1").setValue(sCuil[0]);
-                            } catch(err) {
-                                  other.byId("__input1").setValue("");
-                            }
-                            try{
-                                 other.byId("__input2").setValue(sTotal[1] + "." + sTotal[2]);
-                            } catch(err) {
-                                 other.byId("__input2").setValue("");
-                            }
-                            var linesArray = [];
-                            var defaultVal = {text : "Please select a value"};
-                                linesArray.push(defaultVal)
 
-                            for(var i = 0; i < resultOrError.lines.length; i++ ){
-                                var valToPush = {
-                                    text: resultOrError.lines[i].text
-                                };
-                                linesArray.push(valToPush);
-                            }
-                            sap.ui.core.BusyIndicator.hide();
-                            //dropdown w/text lines
-                            var oDropdownModel = new sap.ui.model.json.JSONModel({
-                                values: linesArray
-                            });
+                    } catch(err) {
 
-                            other.getView().setModel(oDropdownModel, "dropdownSL");
+                        other.byId("__input5").setValue("");
 
-                        })
+                    }
+
+                    var oCuil = /\d{2}[-]\d{8}[-]\d{1}/g;
+                    var sCuil = resultOrError.text.match(oCuil);
+                    var oTotal =/(?:Total\:\s)(?:\w+\s)(\d+)(?:.|,|s)(\d+)/;
+                    var sTotal =resultOrError.text.match(oTotal);
+                    try {
+                        other.byId("__input1").setValue(sCuil[0]);
+                    } catch(err) {
+                        other.byId("__input1").setValue("");
+                    }
+                    try{
+                        other.byId("__input2").setValue(sTotal[1] + "." + sTotal[2]);
+                    } catch(err) {
+                        other.byId("__input2").setValue("");
+                    }
+                    var linesArray = [];
+                    for(var i = 0; i < resultOrError.lines.length; i++ ){
+                        var valToPush = {
+                            text: resultOrError.lines[i].text
+                        };
+                        linesArray.push(valToPush);
+                    }
+                    sap.ui.core.BusyIndicator.hide();
+                    //dropdown w/text lines
+                    var oDropdownModel = new sap.ui.model.json.JSONModel({
+                        values: linesArray
+                    });
+
+                    other.getView().setModel(oDropdownModel, "dropdownSL");
+                    var notMatch = {
+                        text: " "
+                    }
+                    var linesPlace = [];
+                    var linesDate = [];
+                    var lineTotal = [];
+                    var linesCuil = [];
+                    try {
+                        for(var i = 0; i < sPlace.length; i++ ){
+                            var valToPush = {
+                                text: sPlace[i]
+                            };
+                            linesPlace.push(valToPush);
+                        }
+                    }catch(sPlace){
+                        linesPlace.push(notMatch);
+                    }
+                    try{
+                        for(var i = 0; i < sDate.length; i++){
+                            var valToPush = {
+                                text: sDate[i]
+                            };
+                            linesDate.push(valToPush);
+                        }
+                    }catch(sDate){
+                        linesDate.push(notMatch);
+                    }
+                    try{
+                        for(var i = 0; i < sCuil.length; i++){
+                            var valToPush = {
+                                text: sCuil[i]
+                            };
+                            linesCuil.push(valToPush);
+                        }
+                    }catch(sCuil){
+                        linesCuil.push(notMatch);
+                    }
+                    try{
+
+                        for(var i = 0; i < sTotal.length; i++){
+                            var valToPush = {
+                                text: sTotal[i]
+                            };
+                            lineTotal.push(valToPush);
+                        }
+                    }catch(sTotal){
+                        lineTotal.push(notMatch);
+                    }
+                    var oMatchsModel = new sap.ui.model.json.JSONModel({
+                        place : linesPlace,
+                        date : linesDate,
+                        cuil : linesCuil,
+                        total : lineTotal
+                    });
+
+                    other.getView().setModel(oMatchsModel, "matchModel");
+
+
+                })
 
                 },
                 function(err) {
@@ -107,6 +161,8 @@ sap.ui.define([
             control.setValue(model.oData.values);
             model.setData("values", "");
         },
+
+
 
         onClickCopyText: function(oEvent){
             var lineSelected = this.getView().byId("selectRecognizedText").getSelectedItem().getText();
@@ -157,7 +213,7 @@ sap.ui.define([
             });
         },
 
-		/**
+        /**
          * Function to convert a base64 string in a Blob according to the data and contentType.
          *
          * @param sB64Data {String} Pure base64 string without contentType
@@ -190,5 +246,67 @@ sap.ui.define([
         },
 
 
-	});
+
+        onOpenDialog : function (oPressId) {
+            var oView = this.getView();
+            var oDialog = oView.byId("helloDialog");
+            var oInput = oPressId.getParameter("id");
+
+            var oIdModel = new sap.ui.model.json.JSONModel({
+                input : oInput
+            });
+            this.getView().setModel(oIdModel, "buttonIdModel");
+
+
+
+            if (!oDialog) {
+                oDialog = sap.ui.xmlfragment(oView.getId(), "UI5OCRTickets.view.MatchesDialog", this);
+                oView.addDependent(oDialog);
+            }
+            var list;
+            if (oInput.includes("buttonPlace")){
+                list =  this.getView().getModel("matchModel").getProperty("/place/");
+            }else if(oInput.includes("buttonCuil")){
+                list =  this.getView().getModel("matchModel").getProperty("/cuil/");
+            }else if(oInput.includes("buttonDate")){
+                list =  this.getView().getModel("matchModel").getProperty("/date/");
+            }else{
+                list =  this.getView().getModel("matchModel").getProperty("/total/");
+            }
+
+
+            if (list) {
+                var oListModel = new sap.ui.model.json.JSONModel({
+                    listValues: list
+                });
+                this.getView().setModel(oListModel, "matchesList");
+            }
+
+
+            oDialog.open();
+        },
+        onCloseDialog : function () {
+            this.getView().byId("helloDialog").close();
+        },
+
+        onCopyLine : function(oEvent) {
+
+            var itemSelected = oEvent.oSource._$ItemPressed.context.innerText;
+            var idButtonPress = this.getView().getModel("buttonIdModel").oData.input;
+
+            this.getView().byId("helloDialog").close();
+
+            if (idButtonPress.includes("buttonPlace")){
+                this.getView().byId("__input5").setValue(itemSelected);
+            }else if (idButtonPress.includes("buttonCuil")){
+                this.getView().byId("__input1").setValue(itemSelected);
+            }else if (idButtonPress.includes("buttonDate")){
+                this.getView().byId("__input0").setValue(itemSelected);
+            }else{
+                this.getView().byId("__input2").setValue(itemSelected);
+            }
+
+        }
+
+    });
 });
